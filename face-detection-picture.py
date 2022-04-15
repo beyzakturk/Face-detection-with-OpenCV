18f11be00e2742bd865600d711328ab2d934a1ab
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-04-15 17:41:39 +0300

    first commit

---
+++ face-detection-picture.py
@@ -0,0 +1,25 @@
+import cv2
+import imageio
+
+# Installing Cascade
+face_cascade = cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade-eye.xml')
+
+# A function that will perform recognition
+def detect(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame
+
+# The image in the project folder.face and eye detection is performed in the jpg file.
+# Then output.it will be written to jpg file.
+image = imageio.imread('2.jpeg')
+image = detect(frame=image)
+imageio.imwrite('output.jpg', image)
